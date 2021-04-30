7ac4fb741518b31ac23f8a1b0f6138ca1378d279
Julio Canizalez <juliocanizalez@outlook.com>
2021-04-29 22:52:10 -0600

    :construction: [ADD] routes and default for 404

---
+++ src/routes/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from '../containers/Home'
+import PartOne from '../containers/PartOne'
+import PartTwo from '../containers/PartTwo'
+import NotFound from '../containers/NotFound'
+import About from '../containers/About'
+
+
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/uno" component={PartOne} />
+            <Route exact path="/dos" component={PartTwo} />
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+)
+
+
+export default App
