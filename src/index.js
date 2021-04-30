54d67317fb8436cbe53eba791a8afe36099890fe
Julio Canizalez <juliocanizalez@outlook.com>
2021-04-29 23:10:26 -0600

    :sparkles: [ADD] index.js with all necesary imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './routes/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/App.css'
 
 ReactDOM.render(
   <React.StrictMode>
