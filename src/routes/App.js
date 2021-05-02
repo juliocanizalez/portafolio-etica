939b76d583fe3d2b62f6184e80f774b66ffefa4f
Julio Canizalez <juliocanizalez@outlook.com>
2021-05-02 02:33:02 -0600

    :bug: [UPDATE] BrowserRouter now is replaced by HashRouter

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from '../containers/Home'
 import PartOne from '../containers/PartOne'
 import PartTwo from '../containers/PartTwo'
@@ -7,7 +7,7 @@ import NotFound from '../containers/NotFound'
 import About from '../containers/About'
 
 const App = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/uno" component={PartOne} />
@@ -15,7 +15,7 @@ const App = () => (
             <Route exact path="/about" component={About} />
             <Route component={NotFound} />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 )
 
 export default App
