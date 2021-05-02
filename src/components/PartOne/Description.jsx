be35ffa75155f05a5e064fcfb786a0076ef0db08
Julio Canizalez <juliocanizalez@outlook.com>
2021-05-02 00:35:04 -0600

    :hammer: [UPDATE] Part I description

--- src/components/PartOne/Description.jsx
+++ src/components/PartOne/Description.jsx
@@ -4,16 +4,12 @@ import LootiePlayer from '../General/LootiePlayer'
 
 const Description = () => {
     return (
-        <Container>
+        <Container className="mt-4">
             <Row className="justify-content-center align-items-center">
                 <Col sm lg="6">
-                    <h1 className="text-center">
+                    <h1 className="text-center display-4">
                         Parte I
                     </h1>
-                    <h5 className="font-weight-normal text-center">
-                        Plantear dos casos hipotéticos de un Ingeniero en Desarrollo de Software en los que se observe:
-                    </h5>
-                    <p className="text-dark text-center font-weight-light">Un acto humano bueno, un acto humano malo</p>
                 </Col>
                 <Col sm lg="6">
                     <LootiePlayer url='https://assets7.lottiefiles.com/private_files/lf30_WdTEui.json' />
